16d269021f80d9c6dd46681afa5847e11559efd7
OrlovMykhail <dlasteama19@gmail.com>
2026-05-20 11:36:15 +0200

    Aggiunta del calcolo dell'iva e completamento del programma

--- importo.js
+++ importo.js
@@ -1,4 +1,4 @@
-const { calcolaSconto } = require('./calcoli.js');
+const { calcolaSconto, calcolaIva } = require('./calcoli.js');
 
 const args = process.argv.slice(2);
 const importoBase = parseFloat(args[0]);
@@ -9,4 +9,11 @@ if (operazione === 'sconto') {
     const sconto = calcolaSconto(importoBase, percentuale);
     const prezzoScontato = importoBase - sconto;
     console.log(`Prezzo scontato: ${prezzoScontato}`);
+} else if (operazione === 'iva') {
+    const iva = calcolaIva(importoBase, percentuale);
+    const prezzoConIva = importoBase + iva;
+    console.log(`Prezzo con IVA: ${prezzoConIva}`);
 }
+
+module.exports = { calcolaSconto, calcolaIva };
+
